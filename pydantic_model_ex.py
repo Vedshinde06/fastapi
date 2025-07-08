f1ef9b7586f9a984b3ab59f38403b7e3c9a4b7e8
Vedshinde06 <vedantshinde501@gmail.com>
2025-07-08 14:59:26 +0530

    custom functions for validation

--- pydantic_model_ex.py
+++ pydantic_model_ex.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr, AnyUrl
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     
     name: str
+    email: EmailStr
+    Linkedin_URL: AnyUrl
     age: int
     weight: float
-    married: bool
-    allergies: List[str] #To validate that every value in the list is a string
+    married: bool = False
+    allergies: Optional[List[str]] = None #To validate that every value in the list is a string
     contact_details: Dict[str, str]
     
 def insert_patient_data(patient: Patient):
@@ -23,7 +25,8 @@ def update_patient_data(patient: Patient):
     print(patient.age)
     print('updated')    
     
-patient_info = {'name': 'Vedant', 'age': 'thirty', 'weight': '59', 'married': 'True', 'allergies': ['dust', 'red chilli'], 'contact_details': {'email': 'xyz@kmail.com', 'Mob': '900689940'}}
+patient_info = {'name': 'Vedant', 'email': 'vedu@email.com', 'Linkedin_URL': 'https://xyz.com', 'age': '30', 'weight': '59', 'married': 'True', 'allergies': ['dust', 'red chilli'], 'contact_details': {'email': 'xyz@kmail.com', 'Mob': '900689940'}}
+
 patient1 = Patient(**patient_info)
     
 insert_patient_data(patient1)
