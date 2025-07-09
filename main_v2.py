5d25101bfb8a6727cbb93b53b30c161d9b6aa77e
Vedshinde06 <vedantshinde501@gmail.com>
2025-07-09 23:49:41 +0530

    Endpoint create

--- main_v2.py
+++ main_v2.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI, HTTPException, Path, Query
+from pydantic import BaseModel, Field 
+from typing import Annotated, Literal
 import json
 
 app = FastAPI()
+
+class Patient(BaseModel):
+    id: Annotated[str, Field(..., desctiption="ID of the patient", examples={"examples1": {"value": "P001"}})]
+    name: Annotated[str, Field(..., description="Name of the patient")]
+    city: Annotated[str, Field(..., description="City of the patient")]
+    age: Annotated[int, Field(..., description="Age of the patient, must be a positive integer", ge=0, lt=120)]
+    gender: Annotated[Literal["Male", "Female", "Other"], Field(..., description="Gender of the patient")]
+    height: Annotated[float, Field(..., descripiton="Height of the patient in meters", ge=0)]
+    weight: Annotated[float, Field(..., description="Weight of the patient in kg", ge=0)]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        
+        bmi = round(self.weight / (self.height ** 2), 2) 
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return "Underweight"
+        elif 18.5 <= self.bmi < 30:
+            return "Normal"
+        elif self.bmi >30:
+            return "Overweight"
+        else:
+            return "Obese"   
 
 # Function for loading data from json file
 def load_data():
@@ -55,3 +85,10 @@ def sort_patients(sort_by : str = Query(..., description='Sort on the basis of H
     sorted_data = sorted(data.values(), key= lambda x : x.get(sort_by, 0), reverse=sort_order)
     
     return sorted_data
+
+@app.post("/create")
+def create_patient(patient: Patient):
+    data = load_data()
+    
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail="Patient with this ID already exists")
