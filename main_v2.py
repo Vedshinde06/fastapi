ef96947c9d7a24b788db180a93cae8e9f4fb5c22
Vedshinde06 <vedantshinde501@gmail.com>
2025-07-09 22:50:59 +0530

    new endpoint sort

--- main_v2.py
+++ main_v2.py
@@ -36,3 +36,22 @@ def view_patient(
     
     # Correct way to return an error
     raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(sort_by : str = Query(..., description='Sort on the basis of Height, Weight or BMI'), order: str = Query('asc', description = 'Sort in asc or decs order')):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=404, detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'decs']:
+        raise HTTPException(status_code=404, detail='Invalid order select between asc or desc')
+    
+    data = load_data()
+    
+    sort_order= True if order =='decs' else False 
+    
+    sorted_data = sorted(data.values(), key= lambda x : x.get(sort_by, 0), reverse=sort_order)
+    
+    return sorted_data
